16b6ffdfc6a992529cba4f058d61942d7c9f5122
Murillo Pezzuol <murillo.pezzuol@pulselive.com>
2022-10-03 17:29:59 +0100

    interacting between req and res without express js

--- node-basics/app.js
+++ node-basics/app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer( (req, res) => {
     const url = req.url;
+    const method = req.method;
 
     // Setting the type of response to browser, will be a text/html where we can return one html page for example
     res.setHeader('Content-Type', 'text/html');
@@ -15,7 +17,7 @@ const server = http.createServer( (req, res) => {
         <body>
             <h1>Enter a message:</h1>
             <form action="/message" method="POST">
-                <input type="text">
+                <input type="text" name="message">
                     <button type="submit">Send</button>
             </form>
         </body>
@@ -24,17 +26,41 @@ const server = http.createServer( (req, res) => {
         return res.end();
     }
 
+    if (url === '/message' && method === 'POST' ) {
+        const body = [];
+
+        // Request + ON = Event Listener about data chunks.... until to be ready
+        // data is what is comming, like a chunks... piece of piece
+        req.on('data', (chunk) =>{
+            console.log('chunk: ');
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        // End is as a bustop, where we can interact with our chunks
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log('parsedBody: ');
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('./node-basics/message.txt', message);
+        });
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/'); // The browser will redirect use to this location, therefore the '/' path
+        return res.end();
+    };
+
     res.write(`<html>
     <head>
         <title>Index</title>
     </head>
     <body>
-        <h1>Main page running!</h1>
+        <h1>Main Page</h1>
     </body>
     </html>`);
-
-    // Response -> End = Return the response to client
-    res.end();
+    // Here we need to force return, because not, after this line, the others line will execute, and we don't want.
+    return res.end();
 } );
 
 server.listen(3000);
