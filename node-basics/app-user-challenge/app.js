c89fc9b02ad764a1ca411f4fb8e946ef7c0fa282
Murillo Pezzuol <murillo.pezzuol@pulselive.com>
2022-11-25 18:39:24 +0000

    Added Handler Not Found

--- node-basics/app-user-challenge/app.js
+++ node-basics/app-user-challenge/app.js
@@ -15,5 +15,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>');
+});
+
 // Server Running
 app.listen(3000);
