159192264b21140a9419f8d02bf1024ef223c191
Murillo Pezzuol <murillo.pezzuol@pulselive.com>
2022-11-25 14:54:52 +0000

    Adding express and middleware tests

--- node-basics/app-user-challenge/app.js
+++ node-basics/app-user-challenge/app.js
@@ -1,6 +1,21 @@
 const http = require('node:http');
-const routes = require('./routes');
 
-const server = http.createServer(routes.handler);
+const express = require('express');
+const app = express();
 
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+
+    // We need to send to `next` function/middleware that we have, then the middleware below will be executed, because will continue the journey
+    // Without this next command, the console log for another middleware won't be executed.
+    // Without Next, just will return the response/browser.
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+});
+
+const server = http.createServer(app);
 server.listen(3000);
