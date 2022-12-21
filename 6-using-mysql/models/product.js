c7efa247bae8a2b40ed9d6dd0684080e7e9f1770
Murillo Pezzuol <murillo.pezzuol@pulselive.com>
2022-12-21 10:18:26 +0000

    Fetching data in our DB

--- 6-using-mysql/models/product.js
+++ 6-using-mysql/models/product.js
@@ -1,22 +1,7 @@
-const fs = require('fs');
-const path = require('path');
+const db = require('../util/database');
 const Cart = require('./cart');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+const productTableName = 'products';
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -28,46 +13,19 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this; // This means the current product that is coming by request
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-        });
-      } else {
-        this.id = new String( Math.floor(Math.random() * (999999 - 1 + 1)) + 1 );
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
+    
   }
 
   static deleteById(id) {
-    getProductsFromFile(products => {
-      // Just return list of products expect with this ID and then update file with them
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (!err){
-          Cart.deleteProduct(id, product.price);
-        }
-      });
-    });
+  
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    // This return a promise
+    return db.execute(`SELECT * FROM ${productTableName}`);
   }
 
-  static findById(id, cb){
-    getProductsFromFile(products => {
-      // Find from the Array type is like a 'Stream' in Java
-      const product = products.find(p => p.id === id );
-      cb(product);
-    })
+  static findById(id){
+    
   }
 };
