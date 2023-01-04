70dfe11ee289b83cd9d71e8318212722c7465cd8
Murillo Pezzuol <murillo.pezzuol@pulselive.com>
2023-01-04 10:24:43 +0000

    Added middleware to handle the User

--- 7-using-sequelize/app.js
+++ 7-using-sequelize/app.js
@@ -21,6 +21,20 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware that will be called before all others middleware because of the order here.
+// This is simulating one Handler Request comparing between in the Spring in the Java
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            // Adding User object from database into a new field in the Request object of Express JS
+            // If the field don't exist in Request object, then is created
+            req.user = user;
+            next(); // Continue to the next middleware
+        })
+        .catch(err => console.log(err))
+});
+
+// Middlewares - Request Incoming
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -31,9 +45,24 @@ User.hasMany(Product);// This line can be optional
 
 // Using Sequelize - Init config
 sequelize
+    // .sync({force: true}) -- Force update database, creating and deleting table always, not recommended, just in dev/init project
     .sync()
     .then(result => {
-        console.log(result);
+        // Check if exist User ID 1
+        return User.findByPk(1);
+    })
+    .then(user => {
+        // Create User just for tests if the user don't exist
+        if (!user) {
+            User.create({
+                name: 'Murillo Pezzuol',
+                email: 'test@gmail.com'
+            });
+        }
+        return user;
+    })
+    .then(user => {
+        console.log(user);
         app.listen(3000);
     })
     .catch(err => {
