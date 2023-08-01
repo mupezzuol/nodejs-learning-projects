075dc46626579e3803a69642139c764be2995191
Murillo Pezzuol <murillo.pezzuol@pulselive.com>
2023-08-01 10:48:11 +0100

    added mongodb approach

---
+++ 8-using-mongodb/app.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('64c8d204c8afd0ecb1c80c88')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next(); // continue to arrive to another request, but with user in the Request object
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
